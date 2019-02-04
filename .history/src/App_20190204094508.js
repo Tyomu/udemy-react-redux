833a2bba6b1fac88291ae6dfb637738c58b22668
tyomu <t-nomura@mic.jp>
2019-02-04 09:48:52 +0900

    jsx

---
+++ .history/src/App_20190204094508.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return <div >Hello Wold!</div>;
+//   }
+// }
+
+class App extends Component {
+  render() {
+    return React.createElement("div", null, "Hello, world!!!!");
+  }
+}
+
+export default App;
