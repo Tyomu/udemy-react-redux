49f56daec968a08ef2ebfe391a01c0a3c6d45b66
tyomu <t-nomura@mic.jp>
2019-02-04 14:37:22 +0900

    implement store

--- .history/src/index_20190204143224.js
+++ .history/src/index_20190204143224.js
@@ -8,14 +8,7 @@ import reducer from './reducers'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer);
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
